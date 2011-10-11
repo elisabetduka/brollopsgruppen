a4d6cb9d2a29e103e50fd09b8d3ba09ad7c48b41
Hilla <hilla@hilla-1215B.(none)>
2011-10-11 09:44:19 +0200

    new logo and added design

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,4 +1,4 @@
 <?php
 $this->load->view('header_admin');
-echo'<div id="admin_message"><p>Du är nu i admins dashboard. Härifrån kan du redigera alla sidor på Bröllopsgruppen. Välj i menyn ovan vad du vill redigera!</p></div>';
+echo'<div id="admin_message"><p>Du är nu i admins dashboard. Härifrån kan du redigera alla sidor på Bröllopsgruppen. Välj i menyn nedanför vad du vill redigera!</p></div>';
 ?>
