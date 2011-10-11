3da3a45755e1bd9fd70d5f0708648f237fbe57e4
Hilla <hilla@hilla-1215B.(none)>
2011-10-11 09:47:53 +0200

    div for update footer

--- application/views/update_footer.php
+++ application/views/update_footer.php
@@ -5,9 +5,10 @@ $this->load->view('header_admin');
 echo validation_errors();
 
 echo form_open('admin/update_footer/');
+echo '<div id="update_footer">';
 echo form_label('Innehåll', 'content');
 echo form_textarea('content', $logged_in['content'][1]);
 echo '<br />';
 echo form_submit('submit', 'Updatera');
-
+echo '</div>';
 echo form_close();
