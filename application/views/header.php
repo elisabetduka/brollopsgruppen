2dc3af8a72a445270a51eb7c84e74a3acb31dfcb
Hilla <hilla@hilla-1215B.(none)>
2011-10-18 11:57:20 +0200

    adding functionality for rendering footer dynamically instead of static footer

--- application/views/header.php
+++ application/views/header.php
@@ -2,21 +2,70 @@
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" type="text/css" href="/brollopsgruppen/styles/style.css"/>
+	<link rel="shortcut icon" type="image/png" href="/brollopsgruppen/images/tiara.png">
 
 </head>
-
-
+<body>
 <?php 
-$this->load->model('Admin_model');
-$file = $this->Admin_model->get_header();
 
-$link = strstr($file[1]->imglink, 'brollopsgruppen');
+$pages = $header['pages'];
+$link = strstr($header['image'][1]->imglink, 'brollopsgruppen');
 
 ?>
-<div class="header">
-	<img class="backgr" src="/brollopsgruppen/images/wedding-dresses27.jpg" />
-	<a href="<?php base_url().'admin';?>"><img class="logotyp" src="/<?php echo $link;?>" alt="Bröllopsgruppen" /></a>
+	<div class="header">
+		<img class="backgr" src="/brollopsgruppen/images/wedding-dresses27.jpg" />
+		<a href="<?php base_url().'admin';?>"><img class="logotyp" src="/<?php echo $link;?>" alt="Bröllopsgruppen" /></a>
+	</div>
+<?php 
+
+
+
+$url_base_admin = base_url().'admin';
+$url_base_user = base_url().'user';
+if($logged_in['msg'] != NULL){
+	echo "<div id='logout'><a href='$url_base_user/logout'>Logga ut</a></div>";
+} 
+
+if($logged_in['msg'] != NULL){
+?>
+<div id="admin_menu">
+	<ul class="nav">
+		<li><a href="<?php echo $url_base_admin;?>/index">HEM</a></li>
+		<li>REDIGERA FORMULÄR
+			<ul>
+				<!--<li><a href="<?php //echo $url_base_admin;?>/config_contact">Kontaktformulärsinställningar</a></li>-->
+				<li><a href="<?php echo $url_base_admin;?>/create_question">Frågeformulär</a></li>
+			</ul>
+		</li>
+		<li>REDIGERA SIDOR
+			<ul>
+			<?php 
+			
+			foreach($pages as $page){
+				
+				if($page != NULL){
+					echo "<li><a href='$url_base_admin/update_page/$page->id'>$page->title</a></li>";
+				}
+				
+			} 
+			?>
+			</ul>
+		</li>
+		<li>REDIGERA ÖVRIGT
+			<ul>
+				<li><a href="<?php echo $url_base_admin;?>/update_header">Header</a></li>
+				<li><a href="<?php echo $url_base_admin;?>/update_footer">Footer</a></li>
+				<li><a href="">Vänster Sidebar</a></li>
+				<li><a href="">Höger Sidebar</a></li>
+			</ul>
+		</li>
+		<!--<li><a href="<?php// echo $url_base_admin;?>/create_page">SKAPA NY SIDA</a></li>-->
+	</ul>
 </div>
+<?php
+} else {
+?>
+
 <div id="menu">
 	<ul class="nav">
 		<li><a href="<?php base_url().'admin';?>">HEM</a></li>
@@ -24,11 +73,6 @@ $link = strstr($file[1]->imglink, 'brollopsgruppen');
 		<li><a href="">OM BRÖLLOPSGRUPPEN</a></li>
 		<li><a href="">KONTAKT</a></li>
 </div>
-
-<!--Att flytta på så snart vi har en footer, men jag behöver något att styla så länge-->
-<div class="footer">
-	<div class="wrap">
-		<img class="footer_image" src="/brollopsgruppen/images/border.jpg" />
-		<p>© Bröllopsgruppen 2011  |  info@brollopsgruppen.se  |  Tel: 08 - 10 20 30</p>
-	</div>
-</div>
+<?php
+}
+?>
