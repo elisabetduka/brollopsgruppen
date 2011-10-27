f863f2cec7b5fe4a62d3e786ab5c0a9eaebcfdd6
Lickan <linda.lickander@gmail.com>
2011-10-27 15:59:37 +0200

    Editor only visible in admin interface

--- application/views/header.php
+++ application/views/header.php
@@ -7,7 +7,12 @@
 	<link rel="shortcut icon" type="image/png" href="/brollopsgruppen/images/tiara.png">
 	<script type="text/javascript" src="<?php echo $base;?>js/jquery-1.6.2.min.js"></script>
 	<script src="<?php echo $base;?>js/nicEdit.js" type="text/javascript"></script>
-	<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
+	<?php
+	if($main != 'main page'){
+		echo '<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>';
+	} 
+	?>
+	
 						
 </head>
 <body>
