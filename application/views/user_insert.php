5a3cae6b97e887f08f1e071bb7505b4165656c10
Hilla <hilla@hilla-1215B-1215B.(none)>
2011-10-28 11:54:43 +0200

    fixing positioning of editing

--- application/views/user_insert.php
+++ application/views/user_insert.php
@@ -5,7 +5,7 @@ $this->load->view('header');
 echo validation_errors();
 
 echo form_open('user/insert_user/');
-
+echo '<div id="insert_user">';
 echo form_label('E-mail', 'email');
 echo form_input('email');
 echo '<br />';
@@ -16,5 +16,5 @@ echo form_label('Lösenord igen', 'password-repeat');
 echo form_password('password-repeat');
 echo '<br />';
 echo form_submit('submit', 'Logga in');
-
+echo '</div>';
 echo form_close();
