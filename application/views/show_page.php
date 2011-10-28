a4d0eeb3bfcd42f86fc9ebb4caec937c7d0c9339
Lickan <linda.lickander@gmail.com>
2011-10-28 14:15:00 +0200

    Fixed show page for gallery on main page. Now shoving content and pictures if any

--- application/views/show_page.php
+++ application/views/show_page.php
@@ -6,4 +6,29 @@
 </div>
 <div class="content_main">
 	<p><?php $content = $page[1]->content; echo $content; ?></p>
+	<?php 
+		if($page[1]->id == 2){
+			foreach($gallery as $gallery){
+				$ids = $gallery->image_ids;
+				$exploded_id = explode(",", $ids);
+			}
+			$number = count($exploded_id);
+			$number = $number - 1;
+			foreach($show_gallery as $gallery){
+				$img_link = strstr($gallery->imglink, 'brollopsgruppen');
+				$id = $gallery->id;
+				
+				echo '<br />';
+				if($exploded_id != ''){
+					if($exploded_id[$number] == $id){
+						echo "<img src='/$img_link'>";
+					} 
+				}
+			if($number >= 1){
+				$number--;
+			}
+
+			}
+		}
+	?>
 </div>
