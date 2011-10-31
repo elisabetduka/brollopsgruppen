3dc63a376af462ee3549393ac506300a15ac12bf
Hilla <hilla@hilla-1215B-1215B.(none)>
2011-10-31 11:36:00 +0100

    last minute styling ;)

--- application/views/show_contactform.php
+++ application/views/show_contactform.php
@@ -2,22 +2,30 @@
 echo validation_errors();
 
 
-echo form_open("main/send_contactform");
 echo '<h2 class="content_header">Kontakta oss!</h2>';
 echo '<div id="contact_form" class="content_main">';
+echo form_open("main/send_contactform");
 echo form_label('Ämne', 'title');
-echo form_input('title');
 echo '<br />';
+echo form_input('title');
 
+echo '<br />';
+$data = array(
+	'name' => 'content',
+	'style'=> 'width: 500px; border: 1px solid #DAA21 !important;'
+	);
 echo form_label('Meddelande', 'content');
-echo form_textarea('content');
+echo '<br />';
+echo form_textarea($data);
 echo '<br />';
 echo form_label('Email', 'email');
+echo '<br />';
 echo form_input('email');
-//echo '<br />';
+echo '<br />';
 echo form_label('Telefon', 'phone');
+echo '<br />';
 echo form_input('phone');
-//echo '<br />';
+echo '<br />';
 echo form_submit('submit', 'Skicka');
 echo '</div>';
 echo form_close();
